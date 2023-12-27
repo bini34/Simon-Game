f0d581e515b3aa81a97a53a96d3cc1e2cdf87152
Biniyam Ambachew <biniyamambachew@gmail.com>
2023-12-27 14:41:40 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 let buttonColours = ["orange", "red", "yellow", "blue"];
 let gamePattern = [];
 let userClickedPattern = [];
+let started = false;
+let level = 0;
+
+$("inner-inner-circle").click(function(){
+    if (!started){
+        $("level span").text(level);
+        nextSequence();
+        started = true;
+    }
+});
 
 $(".btn").click(function() {
     let userChosenColour = $('this').attr("id");
@@ -11,12 +21,14 @@ $(".btn").click(function() {
 })
 
 function nextSequence(){
+    level++;
+
     let randomNumber = Math.floor(Math.random() * (3 - 0 + 1 ));
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    
+
     playSound(randomChosenColour);
 }
 
