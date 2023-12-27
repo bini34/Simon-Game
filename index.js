a099f6573d91614dc722788d5dec0a22864d6557
Biniyam Ambachew <biniyamambachew@gmail.com>
2023-12-27 09:51:10 +0300

    update

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ let buttonColours = ["orange", "red", "yellow", "blue"];
 let gamePattern = [];
 let userClickedPattern = [];
 
+$(".btn").click(function() {
+    let userChosenColour = $('this').attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+})
+
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * (3 - 0 + 1 ));
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    // animation to fade
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
-$(".btn").on("click", function() {
-    let userChosenColour = $('id')
-})
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function(){
+        $("#" + currentColour).removeClass('pressed');
+      }, 100);
+}
