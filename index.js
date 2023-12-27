0f28abd595c3aab547e27f5a8471b49af6bbb58c
Biniyam Ambachew <biniyamambachew@gmail.com>
2023-12-27 15:22:34 +0300

    update

--- index.js
+++ index.js
@@ -4,24 +4,56 @@ let userClickedPattern = [];
 let started = false;
 let level = 0;
 
-$("inner-inner-circle").click(function(){
+$("#inner-inner-circle").click(function(){
     if (!started){
-        $("level span").text(level);
+        $(".level span").text(level);
         nextSequence();
         started = true;
     }
 });
 
 $(".btn").click(function() {
-    let userChosenColour = $('this').attr("id");
+    let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
 
     playSound(userChosenColour);
     animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+
 })
 
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+
+      console.log("wrong");
+      playSound("wrong");
+
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+    }
+
+}
 function nextSequence(){
+    userClickedPattern = [];
     level++;
+    $("#level-title").text(level);
+
 
     let randomNumber = Math.floor(Math.random() * (3 - 0 + 1 ));
     let randomChosenColour = buttonColours[randomNumber];
@@ -39,7 +71,6 @@ function playSound(name){
 
 function animatePress(currentColour){
     $("#" + currentColour).addClass("pressed");
-
     setTimeout(function(){
         $("#" + currentColour).removeClass('pressed');
       }, 100);
