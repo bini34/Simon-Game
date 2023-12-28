842427f83985be4c1e01d45c9bc8abf6de5529b2
Biniyam Ambachew <biniyamambachew@gmail.com>
2023-12-28 07:59:33 +0300

    update

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function checkAnswer(currentLevel) {
         $("body").removeClass("game-over");
       }, 200);
 
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+      $("#level-title").text("Game Over, Press  A Simon Button to Restart");
       startOver();
     }
 
